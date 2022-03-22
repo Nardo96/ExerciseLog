7f4b2c485b248d48b2f90f228a4b7dd69cc155f6
Bernardo <bernardojavier18@gmail.com>
2022-03-21 20:57:24 -0400

    Converted weight, sets, and reps buttons to textfields.

---
+++ exerciselog_pythonista_v5.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 13 23:19:40 2022
+
+@author: Bernardo
+"""
+from datetime import datetime
+from logging import PlaceHolder
+import ui
+import dialogs
+
+class ExerciseLogGUI():
+    """GUI for entering exercise info and exporting record to file."""
+    def __init__(self):
+        
+        #Set up GUI frame
+        view = ui.View()
+        view.name="Exercise Log"
+        view.background_color = "White"
+        
+        #Set up dropdown for selecting exercise, and entry fields for weight in
+        #lbs, # of sets, and # of reps
+        options = ["Chest Press", "Shoulder Press", "Vertical Row", 
+                   "Lat Pulldown", "Dumbbell", "Leg Press", 
+                   "Leg Extension", "Leg Curl"]
+        self.exercise = ""
+        self.weight = ""
+        self.sets = ""
+        self.reps = ""
+        
+        def exercise_tapped(sender):
+            self.exercise = dialogs.list_dialog(title="Exercise Options", items=options)
+        self.exercise_button = ui.Button(title="Exercise", frame=(150,100,250,40))
+        self.exercise_button.action = exercise_tapped
+        view.add_subview(self.exercise_button)
+        
+        def weight_tapped(sender):
+            self.weight = self.weight_textfield.text
+            
+        self.weight_textfield = ui.TextField(placeholder="Insert weight (lbs)", frame=(150, 180, 250, 40))
+        self.weight_textfield.action = weight_tapped
+        view.add_subview(self.weight_textfield)
+        
+        def sets_tapped(sender):
+            self.sets = self.sets_textfield.text
+        self.sets_textfield = ui.TextField(placeholder="Insert # of Sets", frame=(150, 260, 250, 40))
+        self.sets_textfield.action = sets_tapped
+        view.add_subview(self.sets_textfield)
+
+        def reps_tapped(sender):
+            self.reps = self.reps_textfield.text
+        self.reps_textfield = ui.TextField(placeholder="Insert # of Reps", frame=(150, 340, 250, 40))
+        self.reps_textfield.action = reps_tapped
+        view.add_subview(self.reps_textfield)           
+    
+        def save_record(self, sender):
+            """Save exercise log tuple (datetime, str exercise, int weight,
+            int sets, int reps) to file"""
+            record = (datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"), self.exercise.get(), self.weight.get(), 
+                  self.sets.get(), self.reps.get())
+            with open("ExerciseLog.txt", "a") as f:
+                print(record, file=f, sep="")
+
+        
+        #Button to save record
+        self.save_record_button = ui.Button(title="Save", frame=(150, 450, 40, 40))
+        self.save_record_button.action = save_record
+        view.add_subview(self.save_record_button)
+        view.present("fullscreen")
+        
+ExerciseLogGUI()
+   
